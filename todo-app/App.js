35e6149945415cafa37ebe9102f4e6c468979fc0
Elmer C. Galo <elmer.galo@outlook.com>
2020-08-25 23:57:09 -0400

    Add Header component

--- todo-app/App.js
+++ todo-app/App.js
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
+  // Mock Todo's data
   const [todos, setTodos] = useState([
     {
-      key: 1,
+      key: ' 1',
       text: 'Delectus aut autem',
     },
     {
-      key: 2,
+      key: '2',
       text: 'Quis ut nam facilis et officia qui',
     },
     {
-      key: 3,
+      key: '3',
       text: 'Fugiat veniam minus',
     },
   ]);
 
   return (
     <View style={styles.container}>
+      <Header />
       <View style={styles.content}>
         <View style={styles.list}>
           <FlatList
