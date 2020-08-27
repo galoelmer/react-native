234a2309c1b8eb87fedacaed430766911c54dd8d
Elmer C. Galo <elmer.galo@outlook.com>
2020-08-27 17:27:25 -0400

    Add a drawer or slide navigation menu

--- gamezone/App.js
+++ gamezone/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import Home from './screens/home';
+import HomeScreen from './screens/home';
 import ReviewDetails from './screens/reviewDetails';
+import About from './screens/about';
 import { StatusBar } from 'expo-status-bar';
 import { AppLoading } from 'expo';
 import { useFonts } from 'expo-font';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { getAppLoadingLifecycleEmitter } from 'expo/build/launch/AppLoading';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const HomeStack = () => (
+  <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+    <Stack.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        title: 'MovieZone',
+      }}
+    />
+    <Stack.Screen
+      name="Details"
+      component={ReviewDetails}
+      options={{ title: 'Review Details' }}
+    />
+  </Stack.Navigator>
+);
+
 const screenOptions = {
   headerStyle: {
     backgroundColor: '#003049',
@@ -31,20 +53,10 @@ export default function App() {
   } else {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
-              title: 'MovieZone',
-            }}
-          />
-          <Stack.Screen
-            name="Details"
-            component={ReviewDetails}
-            options={{ title: 'Review Details' }}
-          />
-        </Stack.Navigator>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeStack} />
+          <Drawer.Screen name="About" component={About} />
+        </Drawer.Navigator>
         <StatusBar style="light" />
       </NavigationContainer>
     );
--- gamezone/screens/about.js
+++ gamezone/screens/about.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { globalStyles } from '../styles/globals';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function About() {
+export default function About({ navigation }) {
   return (
-    <View style={globalStyles.container}>
-      <Text>About Screen</Text>
+    <View>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>About</Text>
+      </View>
+
+      <View style={styles.button}>
+        <Button onPress={() => navigation.goBack()} title="Go back home" />
+      </View>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#003049',
+    height: 90,
+  },
+  headerText: {
+    flex: 1,
+    color: '#fff',
+    paddingLeft: 20,
+    paddingBottom: 20,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlignVertical: 'bottom',
+  },
+  button: {
+    marginTop: 30,
+    marginHorizontal: 50,
+  },
+});
